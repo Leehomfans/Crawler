3016a9c63ecffd60dba775379933f97df5cbbd8e
Leehomfans <1051141059@qq.com>
2019-05-28 13:48:24 +0800

    Update craw.py

--- craw.py
+++ craw.py
@@ -7,18 +7,17 @@ def getHtml(url):
     return html
 
 def getArticleUrl(html):
-    reg = r'(https://www.cnblogs.com/'+str_name+'/p/[0-9]+.html)'
+    reg = r'(https://www.cnblogs.com/'+blog_name+'/p/[0-9]+.html)'
     articleUrl = re.findall(reg,html)
     return articleUrl
 
-str_name = input("请输入：");
+blog_name = input("请输入博主昵称：");
 article = []
-htmlStr = getHtml("http://www.cnblogs.com/"+str_name+"/default.html")
+htmlStr = getHtml("http://www.cnblogs.com/"+blog_name+"/default.html")
 for i in range(1,10):
-    html = getHtml("http://www.cnblogs.com/"+str_name+"/default.html?page="+str(i))
+    html = getHtml("http://www.cnblogs.com/"+blog_name+"/default.html?page="+str(i))
     articleUrl = getArticleUrl(html)
     if len(articleUrl)==0:
-        print(i)
         break;
     article = article.__add__(articleUrl)
 
